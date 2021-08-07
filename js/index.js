44fb4f7255471ceec1098cd3316829e267f1b2a3
Sharique Khan <khansharique69.sk@gmail.com>
2021-08-07 17:44:28 +0530

    Dark Mode and Isolation Mode for mobile.

--- js/index.js
+++ js/index.js
@@ -104,7 +104,7 @@ function changeAllColorsForMobile() {
 }
 
 document.onkeydown = function (e) {
-  if (event.keyCode == 123) {
+  if (e.keyCode == 123) {
     return false;
   }
   if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
